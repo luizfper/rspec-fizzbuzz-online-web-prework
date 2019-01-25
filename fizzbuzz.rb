13d26072368cbde511fbfc49d64c50ef82fb21d0
Luiz Pereira <luizfper@gmail.com>
2019-01-25 23:15:07 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  if num%3==0
+    fb="Fizz"
+  end
+  if num%5==0
+    if fb == nil
+      fb =""
+    end
+    fb=fb+"Buzz"
+  end
+  fb
+end
